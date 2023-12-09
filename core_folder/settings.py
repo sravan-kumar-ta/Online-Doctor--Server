d1c6044018c99d98dbe607fe97021e3c385f2bcf
sravan-kumar-ta <sravan0095@gmail.com>
2023-12-09 17:09:28 +0530

    Readme.MD

--- core_folder/settings.py
+++ core_folder/settings.py
@@ -123,7 +123,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=100),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
